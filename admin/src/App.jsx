8d2fb66468c09680ff9a27c53312bd63588ca8bd
Sanyam Munot <sanyammunot03@gmail.com>
2025-09-23 00:32:45 +0530

    admin changes for deployment

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,8 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Login/Login";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  // const url = "http://localhost:4000";
+  const url = "https://foodapp-backend-i5aw.onrender.com";
   return (
     <div>
       <ToastContainer />
